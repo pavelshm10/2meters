fbe89b98d5ebb7b1207c0b59ce5dc8a68f480103
pavel_shmilevich <pavel@triolla.io>
2025-06-06 23:22:00 +0300

    update changes

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import RoomInfo from "./Components/RoomInfo";
+
+function App() {
+  const roomsTypes = [
+    "דירות המצוק",
+    "חדרי מטיילים",
+    "טנטי סטודיו",
+    "הטנטי הצפוני- חדש!",
+    "טנטי פלוס",
+    "Tenty",
+    "מתחם קמפינג",
+  ];
+
+  return (
+    <div>
+      <div>
+        
+      </div>
+      <RoomInfo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/RoomInfo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RoomInfo() {
+    return (
+    <div>
+        {roomsTypes<></>
+    </div>
+  )
+}
+
+export default RoomInfo
